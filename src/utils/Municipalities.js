aefec98dda0cb389a0de23a42847ae36a635ebf3
udaya krishna shrestha <shresthaudayakrishna@gmail.com>
2020-04-02 17:24:56 +0545

    changing municipality options

--- src/utils/Municipalities.js
+++ src/utils/Municipalities.js
@@ -1,14 +1,17 @@
 import React from 'react'
 
 export const Municipality_select = ({Municipality, fun}) => (
-            <select data-live-search="true" data-none-results-text="found no results" title="Please select your location:" className=" selectpicker" id="provience" name="location" onChange={fun}>
+  <React.Fragment>
+    <input list="browsers" name="location" onChange={fun} placeholder='Please select your Municipality.' />
+            <datalist id="browsers" name="location" onChange={fun}>
                    
             {Municipality.map( (data,i) =>
-              <option value={data} key={i} data-tokens={data} name="location" >{data}</option>
+              <option value={data} key={i} name="location" >{data}</option>
                     
             )}
-            </select>
+            </datalist>
             
 
-
+  </React.Fragment>
 );
+
