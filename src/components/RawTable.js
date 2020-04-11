68afb289914ef06e025912724a45b1bae1e0841e
Manish Basnet <manish.basnetnp@gmail.com>
2020-04-11 18:29:49 +0545

    pagination added in world map data table

--- src/components/RawTable.js
+++ src/components/RawTable.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useTable, useExpanded, useSortBy, useFilters } from 'react-table'
+import { useTable, useExpanded, useSortBy, useFilters, usePagination } from 'react-table'
 
 function textFilter(rows, ids, filterValue) {
     return rows.filter((r) =>
@@ -45,20 +45,36 @@ const RegionFilter = (placeholderText) => ({ column: { filterValue, preFilteredR
 export default function RawTable(props) {
     const { columns, data, initialState, onRowClick, filterPlaceholder } = props
 
-    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable(
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow, 
+        page, // Instead of using 'rows', we'll use page,
+    // which has only the rows for the active page
+
+    // The rest of these things are super handy, too ;)
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize }, } = useTable(
         {
             columns,
             data,
             defaultColumn: { Filter: RegionFilter(filterPlaceholder), filter: flatten(textFilter) },
-            initialState,
+            initialState: { pageIndex: 0 },
             getResetExpandedDeps: false
         },
         useFilters,
         useSortBy,
-        useExpanded
+        useExpanded,
+        usePagination
     )
 
     return (
+        <>
+
         <div className="data-table-wrap">
             {headerGroups[0].headers[1].render('Filter')}
             <table className="data-table" {...getTableProps()}>
@@ -74,7 +90,7 @@ export default function RawTable(props) {
                     ))}
                 </thead>
                 <tbody {...getTableBodyProps()}>
-                    {rows.map((row, i) => {
+                    {page.map((row, i) => {
                         prepareRow(row)
                         return (
                             <tr id={`table-${row.original.region}`} {...row.getRowProps()}>
@@ -93,7 +109,56 @@ export default function RawTable(props) {
                     })}
                 </tbody>
             </table>
+            {/* 
+        Pagination can be built however you'd like. 
+        This is just a very basic UI implementation:
+      */}
+      <div className="pagination">
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>{' '}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </button>{' '}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </button>{' '}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>{' '}
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+        <span>
+          | Go to page:{' '}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={e => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0
+              gotoPage(page)
+            }}
+            style={{ width: '100px' }}
+          />
+        </span>{' '}
+        {/* <select
+          value={pageSize}
+          onChange={e => {
+            setPageSize(Number(e.target.value))
+          }}
+        >
+          {[10, 20, 30, 40, 50].map(pageSize => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select> */}
+      </div>
             <div style={{ display: 'none' }}>{rows.length} regions</div>
         </div>
+        </>
     )
 }
